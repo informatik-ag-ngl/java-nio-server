16b0a338a705e144b674f8f35467bd01543616c8
CyB3RC0nN0R <kske@outlook.de>
2020-03-07 19:44:01 +0100

    Changed compiler compliance to 11, added module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * Contains the entire server API.
+ * 
+ * @author Kai S. K. Engelbart
+ * @since java-nio-server v0.1-beta
+ */
+module java.nio.server {
+	
+	exports com.jenkov.nioserver;
+}
